3c46857df950b1073053c722991447956785a37c
tianxudong <tianxudong@tp-link.net>
2015-02-26 17:50:52 +0800

    Add style.css and common.js

--- js/common.js
+++ js/common.js
@@ -3,10 +3,16 @@ window.onload = function() {
 }
 
 $(document).ready(function() {
-            $('#fullpage').fullpage({
+
+    $("#demosMenu").change(function(){
+      window.location.href = $(this).find("option:selected").attr("id") + '.html';
+    });
+    
+    $('#fullpage').fullpage({
                 sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
                 anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
                 menu: '#menu',
                 scrollingSpeed: 1000
-            });
-        });
+    });
+    
+});
