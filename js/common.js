fd568917f0479f62009fec8cee712ba0bed6b768
tianxudong <tianxudong@tp-link.net>
2015-02-27 14:37:51 +0800

    Modify common.js

--- js/common.js
+++ js/common.js
@@ -7,12 +7,5 @@ $(document).ready(function() {
     $("#demosMenu").change(function(){
       window.location.href = $(this).find("option:selected").attr("id") + '.html';
     });
-    
-    $('#fullpage').fullpage({
-                sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
-                anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
-                menu: '#menu',
-                scrollingSpeed: 1000
-    });
-    
+       
 });
